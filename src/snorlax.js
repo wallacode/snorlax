7a2fc2a62114a9d19260790caa7eabfa6f37716d
shota <shota@walla.net.il>
2016-01-27 12:28:55 +0200

    IE8 Support. Thanks to @naortor

--- src/snorlax.js
+++ src/snorlax.js
@@ -28,7 +28,7 @@
          * @type {Object}
          */
         config = {
-            threshold: 400,
+            threshold: 600,
             attrPrefix: 'data-snorlax',
             cssClassPrefix: 'snorlax',
             loadDelta: 1000,
@@ -39,7 +39,6 @@
 
         isOn = true
         ;
-
     /**
      * @constructor
      */
@@ -49,39 +48,46 @@
             this.refreshConfig(_config);
         }
 
+        var elems;
+
+        if (!('indexOf' in Array.prototype)) {
+            __addIndexOfToArrayPrototype();
+        }
+
+        elems = __getElementsByClassName(config.cssClassPrefix);
+
+        q = __nodeListToArray(elems);
+
         if (!config.horizontal) {
-            q = []
-                .slice.call(document.getElementsByClassName(config.cssClassPrefix))
-                .map(__getObjectFromHTMLCollection);
+            for (var i = 0; i < q.length; i++)
+                q[i] = __getObjectFromHTMLCollection(q[i]);
 
             var lastScroll = __getDocumentBottomScroll();
 
-            _.addEventListener(config.event, function () {
+            var action = function() {
                 var t = __getDocumentBottomScroll();
-
-                if (t + config.loadDelta < lastScroll) {
-                    return;
-                } else {
+                if (t + config.loadDelta >= lastScroll) {
                     lastScroll = t;
                     __load(t);
                 }
-            });
-
+            };
+            __eventListener(_,config.event,action);
             __load(lastScroll);
         } else {
-            var wrapper = document.getElementById(config.wrap);
 
-            q = [].slice.call(wrapper.getElementsByClassName(config.cssClassPrefix));
+            var wrapper = document.getElementById(config.wrap);
 
             for (var i = 0; i < q.length; i++)
                 q[i] = __getObjectFromHTMLCollection(q[i]);
 
             var lastScroll = __getEndOfWrapper();
 
-            wrapper.parentElement.addEventListener(config.event, function () {
+            var action = function() {
                 var t = __getEndOfWrapper();
                 __load(t);
-            });
+            };
+
+            __eventListener(wrapper.parentElement,config.event,action);
 
             __load(lastScroll);
         }
@@ -129,16 +135,21 @@
      */
     function __load(scroll){
         for(;isOn && q.length;){
+            updateEdgePosition();
             if (!config.horizontal) {
                 if (q[0].top - config.threshold < scroll) {
                     __show(q[0]);
                     q.shift();
-                } else return;
+                } else {
+                    return;
+                }
             } else {
                 if (q[0].left - config.threshold < scroll) {
                     __show(q[0]);
                     q.shift();
-                } else return;
+                } else {
+                    return;
+                }
             }
         }
     }
@@ -149,14 +160,16 @@
      * @private
      */
     function __show(el){
-        var obj;
+        var obj, res;
 
         if (el.cb){
-            window[el.cb](el);
-        } else {
+            res = window[el.cb](el);
+        }
+
+        if (!el.cb || res) {
             if (el.type === 'img') {
                 obj = new Image();
-                obj.src = el.src;
+                obj.src = res || el.src;
                 obj.alt = el.alt;
                 obj.setAttribute('class', 'snorlax-loaded');
             } else {
@@ -165,15 +178,16 @@
             }
 
             obj.setAttribute('class', el.el.getAttribute('class').replace(config.cssClassPrefix, config.cssClassPrefix + '-loaded'));
-            var otherAttributes = el.el.dataset;
-            var prefix = config.attrPrefix.replace('data-','').replace(/-/g, '');
+            var otherAttributes = el.el.dataset || __getDataAttributes(el.el);
+            var prefix = config.attrPrefix.replace(/^data-/,'').replace(/-/g, '');
             for (var att in otherAttributes){
                 if( otherAttributes.hasOwnProperty( att ) && [prefix+'src',prefix+'alt',prefix+'cb'].indexOf(att.toLowerCase()) === -1 ) {
-                    obj.dataset[att] = otherAttributes[att];
+                    if (obj.dataset)
+                        obj.dataset[att] = otherAttributes[att];
+                    else
+                        obj.setAttribute('data-'+att.replace(/[a-z][A-Z][a-z]/g, function(word, pos) { return word.substr(0,1)+'-'+word.substr(1).toLowerCase() }),otherAttributes[att]);
                 }
             }
-
-
 
             if (el.type === 'img') {
                 obj.onload = function(){
@@ -193,7 +207,8 @@
      * @private
      */
     function __getDocumentBottomScroll(){
-        return (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop + _.innerHeight;
+        var innerHeight = _.innerHeight || document.documentElement.clientHeight;
+        return (document.documentElement && document.documentElement.scrollTop) ? document.documentElement.scrollTop + innerHeight : document.body.scrollTop + innerHeight;
     }
 
     /**
@@ -222,4 +237,88 @@
         };
     }
 
+    /**
+     * return the element data attributes in the form of dataset.
+     * @param element
+     * @private
+     */
+
+    function __getDataAttributes(element) {
+        var dataAttributes = {};
+        for (var i = 0; i < element.attributes.length; i++) {
+            var att = element.attributes[i];
+            if (att.specified && att.name.match(/^data-/) && att.name.match(/^data-./).length > 0) {
+                dataAttributes[att.name.replace(/^data-/,'').replace(/-./g, function(word, pos) { return word.charAt(1).toUpperCase() })] = att.value;
+            }
+        }
+        return dataAttributes;
+    }
+
+    /**
+     * creates elements array out of nodeList.
+     * @param elems
+     * @private
+     */
+    function __nodeListToArray(elems) {
+        var elems_array = [];
+        for (var i = 0; i < elems.length; i++) {
+            elems_array.push(elems[i]);
+        }
+        return elems_array
+    }
+
+    /**
+     * adding event listener to an Object cross-platform.
+     * @param elem
+     * @param event
+     * @param cb
+     * @private
+     */
+    function __eventListener(elem, event, cb) {
+        if (elem.addEventListener) {
+            elem.addEventListener(event, cb);
+        }
+        else {
+            elem.attachEvent("on"+event, cb);
+        }
+    }
+
+    /**
+     * Get element by class name cross-platform.
+     *
+     * @param className
+     * @private
+     */
+    function __getElementsByClassName(className) {
+        if(document.getElementsByClassName) {
+            return document.getElementsByClassName(className);
+        }
+        else {
+            return document.querySelectorAll('.'+className);
+        }
+    }
+
+    /**
+     * Adding 'indexOf' functionality to Array Prototype.
+     *
+     * @private
+     */
+    function __addIndexOfToArrayPrototype() {
+        Array.prototype.indexOf = function(find, i) {
+            if (i===undefined) i= 0;
+            if (i<0) i+= this.length;
+            if (i<0) i= 0;
+            for (var n= this.length; i<n; i++)
+                if (i in this && this[i]===find)
+                    return i;
+            return -1;
+        };
+    }
+
+    function updateEdgePosition() {
+        if (q.length) {
+            q[0].top = q[0].el.getBoundingClientRect().top;
+            q[0].left = q[0].el.getBoundingClientRect().left;
+        }
+    }
 }(window));
